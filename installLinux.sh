b95a8e51243bc1181bb156289e4f515ca0a408a7
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2019-12-18 14:52:40 -0300

    Some more improvments

--- installLinux.sh
+++ installLinux.sh
@@ -3,13 +3,18 @@
 echo 'Copy .vimrc file'
 cp .vimrc ~/
 
-echo 'Copy .bash_profile'
+echo 'Copy .bashrc'
 cp .bashrc ~/
 
 if [ -d '~/.vim/pack/default/start/gruvbox' ]
-then 
+then
     echo 'Cloning gruvbox for Vim'
     git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox 
-else
-    echo 'Gruvbox for Vim already installed'
 fi
+
+echo 'Install Fira Mono Font'
+mkdir FMFonts
+unzip Fira_Mono.zip -d FMFonts
+mkdir /usr/share/fonts/fira-mono
+cp FMFonts/*.ttf /usr/share/fonts
+rm -rf FMFonts
