cd380a4f97cdb5bb33812c6e22aa08170369672a
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2019-12-18 14:16:24 -0300

    Install for linux

---
+++ .bashrc
@@ -0,0 +1,11 @@
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+alias ls='clear; ls -GFhl'
+
+#autocomplete for git branch names
+if [ -f ~/.git-completion.bash ]; then
+    . ~/.git-completion.bash
+fi
+
+export PYTHONDONTWRITEBYTECODE=1
---
+++ installLinux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'Copy .vimrc file'
+cp .vimrc ~/
+
+echo 'Copy .bash_profile'
+cp .bashrc ~/
+
+if [ -d '~/.vim/pack/default/start/gruvbox' ]
+then 
+    echo 'Cloning gruvbox for Vim'
+    git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox 
+else
+    echo 'Gruvbox for Vim already installed'
+fi
