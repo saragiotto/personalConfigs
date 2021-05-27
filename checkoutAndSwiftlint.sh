33b55b1da2d5a69920b9802ee6fe363986f7c05e
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2021-05-27 19:01:13 -0300

    Removing old files

--- checkoutAndSwiftlint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-COMMIT=$1
-DATE=$2
-
-cd ~/Documents/Projects/iamaro
-
-git checkout $COMMIT
-mkdir -p ~/Documents/SublimePrj/SwiftReports
-swiftlint --reporter json > ~/Documents/SublimePrj/SwiftReports/$DATE.json
